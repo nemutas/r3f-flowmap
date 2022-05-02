77a3212c2cd7552daf1cce685b31418cbcd3e4e1
nemutas <inohiro.focru@gmail.com>
2022-05-03 02:44:04 +0900

    add pixel mode

--- src/components/three/postprocessing/FlowmapPass.tsx
+++ src/components/three/postprocessing/FlowmapPass.tsx
@@ -10,6 +10,8 @@ const datas = {
 	power: 0.3,
 	range: 0.1,
 	viscosity: 0.04,
+	isPixel: false,
+	pixel: 20,
 	rgbShift: true
 }
 
@@ -21,10 +23,12 @@ export const FlowmapPass: VFC = () => {
 	gui.addNumericSlider(datas, 'power', 0.1, 0.5, 0.01)
 	gui.addNumericSlider(datas, 'range', 0.1, 0.2, 0.01)
 	gui.addNumericSlider(datas, 'viscosity', 0.01, 0.1, 0.01)
+	gui.addCheckBox(datas, 'isPixel', 'Pixel Mode')
+	gui.addNumericSlider(datas, 'pixel', 10, 50, 10)
 	gui.addCheckBox(datas, 'rgbShift', 'RGB Shift')
 
-	const { gl, size } = useThree()
-	const simulator = useMemo(() => new Simulator(gl, size.width, size.height), [])
+	const { gl, size, viewport } = useThree()
+	const simulator = useMemo(() => new Simulator(gl, size.width, size.height), [gl, size])
 	// pixel effect
 	// const simulator = useMemo(() => new Simulator(gl, 100, 50), [gl])
 
@@ -33,6 +37,9 @@ export const FlowmapPass: VFC = () => {
 			tDiffuse: { value: null },
 			u_motionTexture: { value: null },
 			u_powar: { value: datas.power },
+			u_aspect: { value: viewport.aspect },
+			u_pixelMode: { value: datas.isPixel },
+			u_pixel: { value: datas.pixel },
 			u_shift: { value: datas.rgbShift }
 		},
 		vertexShader: vertexShader,
@@ -54,6 +61,8 @@ export const FlowmapPass: VFC = () => {
 			simulator.compute(normalizedMouse, datas.range, datas.viscosity)
 			passRef.current!.uniforms.u_motionTexture.value = simulator.texture
 			passRef.current!.uniforms.u_powar.value = datas.power
+			passRef.current!.uniforms.u_pixelMode.value = datas.isPixel
+			passRef.current!.uniforms.u_pixel.value = datas.pixel
 			passRef.current!.uniforms.u_shift.value = datas.rgbShift
 		}
 	})
@@ -75,19 +84,29 @@ const fragmentShader = `
 uniform sampler2D tDiffuse;
 uniform sampler2D u_motionTexture;
 uniform float u_powar;
+uniform float u_aspect;
+uniform bool u_pixelMode;
+uniform float u_pixel;
 uniform bool u_shift;
 
 varying vec2 v_uv;
 
 void main() {
-  vec4 motionTexture = texture2D(u_motionTexture, v_uv);
-  vec2 uv = v_uv + -motionTexture.xy * u_powar;
+	vec2 st = v_uv;
+	if (u_pixelMode) {
+		vec2 pixel = vec2(u_aspect * u_pixel, u_pixel);
+		st = floor(v_uv * pixel) / pixel;
+	}
+  vec4 motionTexture = texture2D(u_motionTexture, st);
+	
+	vec2 distortion = -motionTexture.xy * u_powar;
+  vec2 uv = v_uv + distortion;
   vec4 tex = texture2D(tDiffuse, uv);
 
   if (u_shift) {
-    vec2 uv_r = v_uv + -motionTexture.xy * u_powar * 0.5;
-    vec2 uv_g = v_uv + -motionTexture.xy * u_powar * 0.75;
-    vec2 uv_b = v_uv + -motionTexture.xy * u_powar * 1.0;
+    vec2 uv_r = v_uv + distortion * 0.5;
+    vec2 uv_g = v_uv + distortion * 0.75;
+    vec2 uv_b = v_uv + distortion * 1.0;
     float tex_r = texture2D(tDiffuse, uv_r).r;
     float tex_g = texture2D(tDiffuse, uv_g).g;
     float tex_b = texture2D(tDiffuse, uv_b).b;
